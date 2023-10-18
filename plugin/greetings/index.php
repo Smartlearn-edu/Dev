9b44b5428f167725e25dcda663f3e0656c5b9736
Smartlearn-edu <onlymade@gmail.com>
2023-10-18 20:42:27 +0300

    localization

---
+++ plugin/greetings/index.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>;.
+
+/**
+ * @package     local_greetings
+ * @copyright   2023 Your name <your@email>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */ require_once('../../config.php');
+require_once($CFG->dirroot. '/local/greetings/lib.php');
+$context = context_system::instance();
+$PAGE->set_context($context);
+$PAGE->set_url(new moodle_url('/local/greetings/index.php'));
+$PAGE->set_pagelayout('standard');
+$PAGE->set_title($SITE->fullname);
+$PAGE->set_heading(get_string('pluginname', 'local_greetings'));
+
+echo $OUTPUT->header();
+// ...
+//echo get_string('greetingloggedinuser', 'local_greetings', fullname($USER));
+echo local_greetings_get_greeting($USER);
+//echo get_string('greetinguser', 'local_greetings');
+echo local_greetings_get_greeting($USER);
+if (isloggedin()) {
+    echo '<h3>Greetings, ' . fullname($USER) . '</h3>';
+} else {
+    echo '<h3>Greetings, user</h3>';
+}
+echo $OUTPUT->footer();
